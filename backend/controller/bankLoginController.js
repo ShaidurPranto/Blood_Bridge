6ab55fb80bd2f9af4f41e363c52969e1b48779d0
ShaidurPranto <mdshaidurrahmanpranto@gmail.com>
2024-02-01 22:29:18 +0600

    bank home page added

---
+++ backend/controller/bankLoginController.js
@@ -0,0 +1,88 @@
+const databaseConnection = require('../database/databaseConnection');
+const sessionManager = require('../sessionManager');
+
+
+async function bankLogin(req,res)
+{
+    console.log('verifying credentials for bank login');
+    const {email,password} = req.body;
+    console.log('email: ',email);
+    console.log('password: ',password);
+    
+    const query = `SELECT BANKID,EMAIL,PASSWORD,STATUS
+    FROM BLOOD_BANK B JOIN BANK_SIGNUP_REQEUSTS R ON B.REQUESTID=R.REQUESTID
+    WHERE R.EMAIL= :email `;
+    const binds={
+        email: email
+    };
+
+    const result = (await databaseConnection.execute(query,binds)).rows;
+
+    console.log("result of the query");
+    console.log(result);
+    if(result.length == 1)
+    {
+        console.log("found one user with this email");
+        if(result[0]["STATUS"] == 'ACCEPTED')
+        {
+            console.log("bank singup request with this email is accepted by admin");
+            if(result[0]["PASSWORD"] == password)
+            {
+                console.log("password matched");
+
+                const bankID = result[0]["BANKID"];
+                console.log("bank's id is: ",bankID);
+
+                console.log("saving this bank's session");
+                const session = sessionManager.getSession(req);
+                session.bank = {BANKID:bankID};
+
+                res.send({
+                    status: 'successful',
+                    bankid: bankID
+                });
+            }
+            else
+            {
+                console.log("wrong password");
+                res.send({
+                    status: 'wrong password'
+                });
+            }
+        }
+        else if(result[0]["STATUS"] == 'REJECTED')
+        {
+            console.log("bank signup request with this email is rejected ");
+            res.send({
+                status: 'rejected'
+            });
+        }
+        else if(result[0]["STATUS"] == 'PENDING')
+        {
+            console.log("bank singup request with this email is pending");
+            res.send({
+                status: 'pending'
+            });
+        }
+    }
+    else
+    {
+        console.log("no bank found with this email and pass")
+        res.send({
+            status: 'no user'
+        });
+    }
+    
+}
+
+
+async function testSession(req,res)
+{
+    
+}
+
+async function navigateToHomePage(req,res)
+{
+
+}
+module.exports = {bankLogin,testSession,navigateToHomePage};
--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,27 @@
 const express = require('express');
 const path = require('path');
-
+const session = require('express-session');
 
 const app = express(); 
+
+const sessionConfig = {
+  name: 'BloodBridge-session',
+  secret: 'prantoabc',
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    secure: false, // Set to true if using HTTPS
+    httpOnly: true,
+  },
+};
+
 app.use(express.json());
 
 app.use(express.static('../frontend'));
 app.use(express.static('../frontend/css'));
 app.use(express.static('../pictures'));
 
+app.use(session(sessionConfig));
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '../views'));
@@ -16,13 +29,11 @@ app.set('views', path.join(__dirname, '../views'));
 
 const port = 3000;
 
+//requests for rendering ejs pages
 
 app.get('/', (req, res) => {
     res.render('index', { message: 'Hello, World!' });
   });
-  // app.get('/userHomePage/isDonor_response_data["name"]', (req, res) => {
-  //   res.render('userHomePage'); 
-  // });
   
   app.get('/UserHomePageForDonor', (req, res) => {
     const name = req.query.name;
@@ -30,7 +41,6 @@ app.get('/', (req, res) => {
     const userid = req.query.userid;
     res.render('userHomePage', { name: name,userid: userid});
 });
-
 
   app.get('/NonDonorUserHomePage',(req,res)=>{
     const name = req.query.name;
@@ -54,19 +64,27 @@ app.get('/', (req, res) => {
     res.render('bloodRequest',{userid: userid });
   
   });
-  
 
+//request to navigate to admin page
+app.get('/admin', (req, res) => {
+  console.log("Navigating user to the admin page");
+  res.sendFile('htmlPages/adminHome.html', { root: '../frontend' });
+});
+
+
+//
 const userLoginRouter = require('./router/userLoginRouter');
 const userSignupRouter = require('./router/userSignupRouter');
 const userHomePageRouter = require('./router/userHomePageRouter');
+const bankLoginRouter = require('./router/bankLoginRouter');
 
 
 app.use('/userLogin',userLoginRouter);
 app.use('/userSignup',userSignupRouter);
 app.use('/userHomePage',userHomePageRouter);
+app.use('/bankLogin',bankLoginRouter);
 
 
 app.listen(port,()=>{
     console.log(`open http://localhost:${port}`);
 });
-
